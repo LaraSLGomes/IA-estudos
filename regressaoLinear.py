e10e0d15e283b1e2f63732bfe7915610eed18bc2
LaraSLGomes <larastephanny05@gmail.com>
2026-08-13 20:19:27 -0300

    aulas

--- regressaoLinear.py
+++ regressaoLinear.py
@@ -11,3 +11,7 @@ xbar = np.mean(xdata)
 print("a media de X é:", xbar)
 ybar = np.mean(ydata)
 print("a media de Y é:", ybar)
+b_1 = np.sum((ydata-ybar) * (xdata-xbar)) / np.sum((xdata-xbar)**2)
+print("o valor do coeficiente angular:", b_1)
+b_0 = ybar - b_1 * xbar
+print("o intercepto é:", b_0)

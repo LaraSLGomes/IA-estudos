c0f6e19216678f1b648248b3dc2da30d3c07d055
LaraSLGomes <larastephanny05@gmail.com>
2026-08-25 20:10:27 -0300

    aula

--- RL.py
+++ RL.py
@@ -2,13 +2,13 @@ import numpy as np
 
 x1 = np.array([2, 8, 11, 10, 8, 4, 2, 2, 9, 8])
 x2 = np.array([50, 110, 120, 550, 295, 200, 375, 52, 100, 300])
-y = np.array([9.95, 24.45, 31.75, 34, 25.02, 18.86, 14.38, 9.6, 24.35, 27.5])
+y = np.array([9.95, 24.45, 31.75, 34, 25.02, 16.86, 14.38, 9.6, 24.35, 27.5])
 
 print(x1.shape[0])
 X = np.column_stack((np.ones(x2.shape[0]), x1, x2))
 class MRegression:
     def __init__(sel, x, y):
-        self.x = x
+        self.x = X
         self.y = y
         sel.beta = None
     def fit(self):
@@ -22,7 +22,7 @@ class MRegression:
         return X_new @ self.beta
 
 X = np.column_stack((x1, x2))
-modelo = MRegression()
+modelo = MRegression(X, y)
 modelo.fit()
 print("Os parametros sao: ", modelo.beta)
 y_pred = modelo.predict(X)

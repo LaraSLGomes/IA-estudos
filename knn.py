b37d69a46589439e488bd395c421d0ef831c65ac
LaraSLGomes <larastephanny05@gmail.com>
2026-09-03 19:17:16 -0300

    aula

--- knn.py
+++ knn.py
@@ -1,5 +1,13 @@
 import numpy as np
-class KNN:
 
-    def__init__(self, k=5, task="classification"):
+class KNN:
+    def __init__(self, k=5, task="classification"):
+         self.k = k
+         self.task = task
+    def fit(self, X_train, y_train):
+        #metodo do treinamento da maquina
+        self.X_train = X_train
+        self.y_train = y_train
+    def calculate_distances(self, x1, x2):
+        return np.sqrt(np.sum((x1-x2)**2)) #euclidiana
     
